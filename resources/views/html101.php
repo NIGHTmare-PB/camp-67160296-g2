8f365689fab15951bc7329124ef8c388a8da46cb
NIGHT-PB <samung060259@gmail.com>
2025-12-01 17:19:47 +0700

    workshop.php

---
+++ resources/views/html101.php
@@ -0,0 +1,48 @@
+<!-- resources/viwes/html101.blade/php -->
+ <!DOCTYPE html>
+ <html>
+        <head>
+            <meta charset="utf-8" />
+            <title>Page Title</title>
+            <link rel="stylesheet" href="xss/bootstrap.css" />
+        </head>
+
+        <body>
+           <h1>Workshop #HTML-FROM</h1>
+            <div class="container pt-5">
+                <FROM>
+                <div class="row align-item-center">
+                        <div class="col-4">
+                            <label for="fname" class="col-form-label">
+                                ชื่อ :
+                                </label>
+                        </div>
+                        <div class="col-6">
+                            <input type="text" id="fname" class="form--control">
+                        </div>
+
+                        <div class="row">
+                            <div class="col-4">
+                                <label for="" class="">
+                                    นามสกุล :
+                            </label>
+                        </div>
+                            <div class="col-6">
+                                    <input type="text" id="fname" class="form--control">
+                            </div>
+
+                        <div class="row">
+                            <div class="col-4">
+                            <label for="" class="">
+                            วัน/เดือน/ปีเกิด :
+                            </label>
+                        </div>
+                        <div class="col-6">
+                            <input type="date" id="bdate" class="form--control">
+                        </div>  
+             </div>                    
+            </div>
+            </FROM>
+        </div>
+    </body>
+</hrml>
